5d30bc40b75638183c179c4a8c4d5b60d5eb4b54
xuziwang <xuziwang@xhg.com>
2019-03-07 11:24:51 +0800

    优化查询语句，增加limit,增加原生sql语句的支持

--- src/main/java/com/yahier/date/repository/UserInfoRepository.java
+++ src/main/java/com/yahier/date/repository/UserInfoRepository.java
@@ -15,8 +15,9 @@ public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
     List<UserInfo> getListBy(int gender);
 
     /**
+     * mark jpa不支持limit查询，修改 此条查询语句为原生查询
      * 根据姓名和手机号码，查找是否有已经存在   测试OK，只是返回列表不够好，应该返回
      */
-    @Query("select t from UserInfo t where t.name = ?1 and t.phoneNo=?2")
+    @Query(value = "SELECT * FROM UserInfo t WHERE t.name = ?1 AND t.phoneNo=?2 LIMIT 1", nativeQuery = true)
     List<UserInfo> getListBy(String name, String phoneNo);
 }
