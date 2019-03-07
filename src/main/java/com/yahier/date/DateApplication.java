a6f957bfbe9ab36116e2df6bc2a0d55485ade92e
xuziwang <xuziwang@xhg.com>
2019-03-07 11:59:45 +0800

    增加启动时的欢迎页面

--- src/main/java/com/yahier/date/DateApplication.java
+++ src/main/java/com/yahier/date/DateApplication.java
@@ -2,12 +2,22 @@ package com.yahier.date;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/")
 @SpringBootApplication
 public class DateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DateApplication.class, args);
+    }
 
+    @RequestMapping("/")
+    public String test1(HttpServletRequest request) {
+        return "/welcome";
+    }
 }
