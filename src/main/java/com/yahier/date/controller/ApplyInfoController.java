1e1f9278fe019f57be5e2dd0e7b7045a95dfc880
xuziwang <xuziwang@xhg.com>
2019-03-26 16:23:03 +0800

    增加处理jsonString

--- src/main/java/com/yahier/date/controller/ApplyInfoController.java
+++ src/main/java/com/yahier/date/controller/ApplyInfoController.java
@@ -6,7 +6,9 @@ import com.yahier.date.pojo.Success;
 import com.yahier.date.pojo.TestItem;
 import com.yahier.date.repository.ApplyInfoRepository;
 import com.yahier.date.table.ApplyInfo;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +21,15 @@ public class ApplyInfoController {
     @Autowired
     private ApplyInfoRepository applyInfoRepository;
 
+    public String name;
+
     /**
      * 报名
      */
     @RequestMapping(value = "/join", produces = {"application/json;charset=UTF-8"})
     public BaseResp join(ApplyInfo applyInfo) {
+
+        System.out.println("name:" + name);
         String verifyErrorResult = DataUtils.verify(applyInfo);
         if (verifyErrorResult != null) {
             return new Fail(verifyErrorResult);
@@ -31,6 +37,15 @@ public class ApplyInfoController {
             applyInfoRepository.save(applyInfo);
             return new Success();
         }
+    }
+
+    /**
+     * 报名 这样才接收到了 原生的 json字符串
+     */
+    @RequestMapping(value = "/join2", produces = {"application/json;charset=UTF-8"})
+    public BaseResp join2(@RequestBody JSONObject jsonParam) {
+        System.out.println("json2" + jsonParam.toJSONString());
+        return new Success();
     }
 
     /**
@@ -76,4 +91,11 @@ public class ApplyInfoController {
         return applyInfoRepository.listForUser(userId);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
